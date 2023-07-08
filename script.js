b0f4ae2c577528e7cff8f9d7969b6311b9611f5c
Sanjeet Rai <raisanjeet8896@gmail.com>
2023-07-08 14:04:41 +0530

    second page added

--- script.js
+++ script.js
@@ -4,20 +4,21 @@ const form = document.querySelector(".sign-up");
 form.addEventListener("submit", (e) => {
   console.log("working");
   e.preventDefault();
-  mail.value = "";
 
   validateEmail();
 });
 
 const pattern = /^[a-zA-Z0-9][\w]{0,15}@[a-zA-Z]{4,12}\.[a-zA-Z]{3}$/;
+const emailVal = mail.value;
 const validateEmail = () => {
-  const emailVal = mail.value;
-
   if (emailVal === "") {
     setErrorMsg(mail, "Email address cannot be empty");
-  } else if (emailVal.match(pattern)) {
-    setErrorMsg(mail, "Thanks to subscribing to our newsletter");
-  } else {
+  }
+  // } else if (emailVal.match(pattern)) {
+  //   setErrorMsg(mail);
+
+  // }
+  else {
     setErrorMsg(mail, "Please provide a valid email address");
   }
 };
@@ -28,3 +29,16 @@ function setErrorMsg(input, errormsgs) {
   formControl.className = "form-control error";
   msg.textContent = errormsgs;
 }
+
+const firstPage = document.querySelector(".first-page");
+const secondPage = document.querySelector(".second-page");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("submit", (e) => {
+  e.preventDefault.default();
+
+  if (mail.value.match(pattern)) {
+    firstPage.style.display = "none";
+    secondPage.style.display = "flex";
+  }
+});
