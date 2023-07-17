f22a79eacea2c1391e24c2f1ffdd28e496f79cb3
Sanjeet Rai <raisanjeet8896@gmail.com>
2023-07-17 14:01:19 +0530

    working on js

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-const form = document.querySelector(".sign-up");
-var firstPage = document.getElementById("first-page");
-var secondPage = document.getElementById("second-page");
-var mail = document.querySelector(".email-container");
-var email = mail.value;
+// const form = document.querySelector(".sign-up");
+// var firstPage = document.getElementById("first-page");
+// var secondPage = document.getElementById("second-page");
+// var mail = document.querySelector(".email-container");
+// var email = mail.value;
 
-form.addEventListener("submit", (e) => {
-  console.log("working");
+const inputEmail = document.querySelector(".email-container");
+const dismissBtn = document.querySelector("#dismiss-btn");
+const subscribeBtn = document.querySelector("#sub-btn");
+const formDiv = document.querySelector("#form-div");
+const thanksDiv = document.querySelector("#second-page");
+const emailError = document.querySelector(".email-error");
+
+let emailValue = "";
+
+subscribeBtn.addEventListener("click", (e) => {
   e.preventDefault();
-
-  //validate - email
-  if (validateEmail(email)) {
-    firstPage.style.visibility = "hidden";
-    secondPage.style.display = "visible";
-  } else {
-    setErrorMsg("valid email required");
+  const emailValue = inputEmail.value;
+  const validEmail = validateEmail(emailValue);
+  if (!validEmail) {
+    inputEmail.classList.add("email-check");
+    emailError.classList.remove("keep-hidden");
+    return;
   }
+  inputEmail.classList.remove("email-check");
+  emailError.classList.add("keep-hidden");
+  formDiv.classList.add("keep-hidden");
+  thanksDiv.classList.remove("keep-hidden");
+  document.querySelector(".user-email").textContent = emailValue;
+  inputEmail.value = "";
 });
 
-function validateEmail(email) {
-  var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return pattern.test(email);
-}
+dismissBtn.addEventListener("click", () => {
+  thanksDiv.classList.add("keep-hidden");
+  formDiv.classList.remove("keep-hidden");
+});
 
-function setErrorMsg(errormsgs) {
-  const msg = document.getElementById("error-msg");
-  msg.textContent = errormsgs;
-}
-
-function handleDismiss() {
-  console.log("dismiss button working");
-  secondPage.style.visibility = "hidden";
-  firstPage.style.visiblity = "visible";
+function validateEmail(input) {
+  const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (input.match(pattern)) {
+    return true;
+  } else {
+    return false;
+  }
 }
